539b0e2e83dddc57d14f067e509ab8782c3c9d3b
AFIF IKRAM <afifikramm@gmail.com>
2026-01-03 17:25:48 +0800

    Added express framework

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 8080;
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    // Tukar mesej ini supaya ada nama anda.
+    // Ini penting supaya pensyarah tahu ini server anda bila buka website nanti.
+    res.send('Hello World!') 
+})
+
+app.listen(port, () => {
+    // Saya betulkan tanda backticks (`) di sini supaya variable ${port} berfungsi
+    console.log(`Example app listening on port ${port}`)
+})
